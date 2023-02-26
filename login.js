04d143479acc2ff493aa172b4c69b320147f1698
wtrewa <psaurabh574@gmail.com>
2023-02-26 16:01:11 +0530

    login

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 let baseURL = "http://localhost:3000";
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
+let currentUser = localStorage.getItem("currentUser") || null;
 // navbar start-------------------------------------------------------------------
 let home__searchBar = document.getElementById("home__searchBar");
 let home__cart = document.getElementById("home__cart");
@@ -8,42 +8,50 @@ let home__logo = document.getElementById("home__logo");
 let home__cartLength = document.getElementById("home__cartLength");
 let home__purchase = document.getElementById("home__purchase");
 let login__signup = document.getElementById("login__signup");
-
-login__signup.addEventListener("click", () => {
-  window.location.href = "signup.html";
-});
-
+if (currentUser) {
+  login__signup.innerText = `Hi, ${currentUser}`;
+} else {
+  login__signup.addEventListener("click", () => {
+    window.location.href = "signup.html";
+  });
+}
 home__purchase.addEventListener("click", () => {
   window.location.href = "purchase.html";
 });
-
 home__logo.addEventListener("click", () => {
   window.location.href = "index.html";
 });
-
 home__searchBar.addEventListener("click", () => {
   window.location.href = "products.html";
 });
-
 home__cart.addEventListener("click", () => {
   window.location.href = "cart.html";
 });
-
 window.addEventListener("load", () => {
   home__cartLength.innerText = cart.length;
 });
-
 // navbar End------------------------------------------------------------------------
-
 // login form start -------------------------------------------------------------------------
-
 let login__form = document.getElementById("login__form");
-let login__username = document.getElementById("login__username");
+let login__email = document.getElementById("login__email");
 let login__password = document.getElementById("login__password");
-
 login__form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(login__username.value, login__password.value);
+  loginUser(login__email.value, login__password.value);
 });
-
-// login form end -------------------------------------------------------------------------
+function loginUser(email, password) {
+  fetch(`${baseURL}/users/${email}`)
+    .then((req) => req.json())
+    .then((res) => {
+      console.log(res);
+      if (res.id == email && res.pass == password) {
+        alert(`Welcome back ${res.name}`);
+        currentUser = res.name;
+        localStorage.setItem("currentUser", res.name);
+        localStorage.setItem("currentUserId", res.id);
+        window.location.href = "index.html";
+      } else {
+        alert(`Something went wrong!, please try again`);
+      }
+    });
+}
--- signup.js
+++ signup.js
@@ -1,13 +1,5 @@
-// let adminAccess = document.getElementById("adminAccess");
-// let adminFormCont = document.getElementById("adminFormCont");
-// let adminLoginForm = document.getElementById("adminLoginForm");
-// let adminFormClose = document.getElementById("adminFormClose");
-// let adminFormUsername = document.getElementById("adminFormUsername");
-// let adminFormPassword = document.getElementById("adminFormPassword");
-
 let baseURL = "http://localhost:3000";
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
 // navbar start-------------------------------------------------------------------
 let home__searchBar = document.getElementById("home__searchBar");
 let home__cart = document.getElementById("home__cart");
@@ -15,55 +7,71 @@ let home__logo = document.getElementById("home__logo");
 let home__cartLength = document.getElementById("home__cartLength");
 let home__purchase = document.getElementById("home__purchase");
 let home__login = document.getElementById("home__login");
-
 home__login.addEventListener("click", () => {
   window.location.href = "login.html";
 });
-
 home__purchase.addEventListener("click", () => {
   window.location.href = "purchase.html";
 });
-
 home__logo.addEventListener("click", () => {
   window.location.href = "index.html";
 });
-
 home__searchBar.addEventListener("click", () => {
   window.location.href = "products.html";
 });
-
 home__cart.addEventListener("click", () => {
   window.location.href = "cart.html";
 });
-
 window.addEventListener("load", () => {
   home__cartLength.innerText = cart.length;
 });
-
 // navbar End------------------------------------------------------------------------
-
 // signup form start --------------------------------------------------------------------
-
 let signup__form = document.getElementById("signup__form");
-let signup__username = document.getElementById("signup__username");
 let signup__password = document.getElementById("signup__password");
 let signup__name = document.getElementById("signup__name");
 let signup__email = document.getElementById("signup__email");
-
+let signup__confirmPassword = document.getElementById(
+  "signup__confirmPassword"
+);
+let existingUsers = [];
+let currentUser = localStorage.getItem("currrentUser") || null;
 signup__form.addEventListener("submit", (e) => {
   e.preventDefault();
   let userObj = {
-    id: signup__username.value,
+    id: signup__email.value,
     pass: signup__password.value,
     name: signup__name.value,
-    email: signup__email.value,
     history: [],
-    cart: [],
   };
-
-  addUser(userObj);
+  if (signup__password.value == signup__confirmPassword.value) {
+    fetch(`${baseURL}/existingUsers`)
+      .then((req) => req.json())
+      .then((res) => {
+        existingUsers = [...res];
+        let flag = false;
+        flag = existingUsers.some((item) => {
+          if (item.id == signup__email.value) {
+            flag = true;
+            return flag;
+          }
+          flag = false;
+          return flag;
+        });
+        if (flag) {
+          alert("User already exists");
+        } else {
+          alert("User Successfully Created");
+          addUser(userObj);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    alert("Please check your password");
+  }
 });
-
 function addUser(data) {
   //   console.log(data);
   fetch(`${baseURL}/users`, {
@@ -76,10 +84,25 @@ function addUser(data) {
     .then((req) => req.json())
     .then((res) => {
       console.log(res);
+      window.location.href = "login.html";
+      //   currentUser = res.name;
+      //   localStorage.setItem("currentUser", currentUser);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  fetch(`${baseURL}/existingUsers`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id: data.id }),
+  })
+    .then((req) => req.json())
+    .then((res) => {
+      console.log(res);
     })
     .catch((err) => {
       console.log(err);
     });
 }
-
-// signup form End------------------------------------------------------------------------
